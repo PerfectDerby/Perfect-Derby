e7512bab7b88853882d66652ebf17c5c241ca175
PerfectDerby Deployer <deploy@perfectderby.com>
2026-06-11 00:22:43 -0400

    Deploy Secure Build

---
+++ assets/index-CRZARCUD.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-S-EhM7Ul.js","./index-DGoMKXoD.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-DCoRsN-h.js";import{r as o}from"./index-DGoMKXoD.js";const _=o("Browser",{web:()=>e(()=>import("./web-S-EhM7Ul.js"),__vite__mapDeps([0,1]),import.meta.url).then(r=>new r.BrowserWeb)});export{_ as Browser};
